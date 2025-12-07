d342c9149d9c3c90a73f0cf788d0d05ebe7a77d1
JessiRs <tqap3132@gmail.com>
2025-12-06 22:00:41 -0500

    Test BL

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,21 +1,31 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 
 public class Login {
-    public String NameUser = "admin";
-    public String Paassword="admin";
-
+    Dotenv dotenv = Dotenv.configure().load();
+    String user = dotenv.get("USER_TEST");
+    String pass = dotenv.get("PASS_TEST");
 
     @Test
-    public void Login() throws InterruptedException {
+    public void LoginTrue() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://quotes.toscrape.com");
+        System.out.println("Usuario: " + user);
+        System.out.println("Password: " + pass);
         driver.findElement(By.cssSelector("a[href='/login']")).click();
-        driver.findElement(By.id("username")).sendKeys(NameUser);
-        driver.findElement(By.id("password")).sendKeys(Paassword);
+        driver.findElement(By.id("username")).sendKeys(user);
+        driver.findElement(By.id("password")).sendKeys(pass);
+        Thread.sleep(2000);
         driver.findElement(By.cssSelector("input[value=Login]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("a[href='/logout']")).click();
+        Thread.sleep(4000);
         driver.quit();
 
     }
 }
+
